515b8385395f9da89346a99c8c683ce6f83c27fc
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-06 23:18:46 +0300

    feature: onDestroy, fanficDetails

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {APIResponse, Fanfic} from "../../models";
 import {HttpService} from "../../services/http.service";
 import {ActivatedRoute, Params, Router} from "@angular/router";
@@ -9,12 +9,13 @@ import {Subscription} from "rxjs";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   // @ts-ignore
   public sort: string;
   // @ts-ignore
   public fanfics: any;
-
+  private routeSub: Subscription = new Subscription;
+  private fanficSub: Subscription = new Subscription();
 
   constructor(
     private httpService: HttpService,
@@ -22,7 +23,7 @@ export class HomeComponent implements OnInit {
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) =>{
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) =>{
       if (params['fanfic-search']){
         this.searchFanfics('id', params['fanfic-search']);
       } else {
@@ -32,7 +33,7 @@ export class HomeComponent implements OnInit {
   }
 
   searchFanfics(sort:string, search?: string): void{
-    this.httpService
+    this.fanficSub = this.httpService
       .getFanficList(sort)
       .subscribe((fanficList: APIResponse<Fanfic>) =>{
         this.fanfics = fanficList;
@@ -42,6 +43,15 @@ export class HomeComponent implements OnInit {
 
   openFanficDetails(id:string): void{
     this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy():void{
+    if (this.fanficSub){
+      this.fanficSub.unsubscribe()
+    }
+    if (this.routeSub){
+      this.routeSub.unsubscribe()
+    }
   }
 
 
