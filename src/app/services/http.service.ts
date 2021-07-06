d3029e4812e337356b6445fdef5645fdb752d49b
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-06 23:19:04 +0300

    feature: getFanficDetails by ID

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {forkJoin, Observable} from "rxjs";
 import {environment as env} from 'src/environments/environment'
 import {APIResponse, Fanfic} from "../models";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +26,17 @@ export class HttpService {
       params: params,
     });
   }
+
+  getFanficDetails(id: number): Observable<Fanfic>{
+    const fanficInfoRequest = this.http.get(`${env.BASE_URL}/fanfic/${id}`);
+
+    return forkJoin({
+      fanficInfoRequest
+    }).pipe(map((resp: any) =>{
+      return {
+        ...resp['fanficInfoRequest']
+      };
+    }))
+  }
 }
+
