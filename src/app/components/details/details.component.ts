077bd2b395b151694e2c8f8cf03bea084507118c
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-06 23:18:03 +0300

    init commit

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Fanfic} from "../../models";
+import {Subscription} from "rxjs";
+import {HttpService} from "../../services/http.service";
+import {ActivatedRoute, Params} from "@angular/router";
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit, OnDestroy {
+  fanficRating = 0;
+  // @ts-ignore
+  fanficId: number;
+  // @ts-ignore
+  fanfic: Fanfic;
+  // @ts-ignore
+  routeSub: Subscription;
+  // @ts-ignore
+  fanficSub: Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) { }
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) =>{
+      this.fanficId = params['id'];
+      this.getFanficDetails(this.fanficId);
+    })
+  }
+
+  getFanficDetails(id:number):void{
+    this.fanficSub = this.httpService
+      .getFanficDetails(id)
+      .subscribe((fanficResp: Fanfic) =>{
+        this.fanfic = fanficResp;
+
+        setTimeout(() => {
+          this.fanficRating = this.fanfic.avgRating;}, 1000);
+        });
+
+  }
+
+  getColor(value: number): string{
+    if (value >= 4){
+      return '#5ee432'
+    } else if (value >= 3){
+      return '#fffa50'
+    }
+    return '#f60000'
+  }
+
+  ngOnDestroy():void {
+    if(this.fanficSub){
+      this.fanficSub.unsubscribe();
+    }
+    if(this.routeSub){
+      this.routeSub.unsubscribe();
+    }
+  }
+
+}
