ec5a0c15a04ba0ab2d5c1f29c622b611508bfacc
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-07 23:38:00 +0300

    fix: cloudinary delete

--- src/app/components/add-fanfic/add-fanfic.component.ts
+++ src/app/components/add-fanfic/add-fanfic.component.ts
@@ -22,8 +22,6 @@ export class AddFanficComponent implements OnInit {
   // @ts-ignore
   picUrl: string;
 
-  declare cloudinary:any
-
   constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
