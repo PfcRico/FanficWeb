2ee810ff57056c95eefdc77edf335d9ed946d8ab
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-07 23:28:30 +0300

    heroku commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,6 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'fanficWeb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // @ts-ignore
     expect(app.title).toEqual('fanficWeb');
   });
 
