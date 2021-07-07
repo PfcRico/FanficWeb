61bd1b5813c977b82fae22389d8365204cb89e11
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-07 23:52:06 +0300

    heroku commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("./dist/fanfic-web"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/fanfic-web" });
+  res.sendFile("index.html", { root: "dist/fanfic-web/src/app" });
 });
 
 app.listen(process.env.PORT || 8080);
