b14c4f92d93fe7385685e2ed97f4402de75ef117
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-11 19:45:59 +0300

    heroku credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  // BASE_URL: 'https://fanfic-backend.herokuapp.com'
-  BASE_URL: 'http://localhost:9191'
+  BASE_URL: 'https://fanfic-backend.herokuapp.com'
+  //BASE_URL: 'http://localhost:9191'
 };
 
 /*
