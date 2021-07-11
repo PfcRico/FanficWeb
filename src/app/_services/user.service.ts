84393ed9533e5787f5fbe40d88345e0937654249
Barys <79482411+PfcRico@users.noreply.github.com>
2021-07-11 22:28:19 +0300

    fix: api endpoint

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://fanfic-web.herokuapp.com/';
+const API_URL = 'https://fanfic-backend.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
